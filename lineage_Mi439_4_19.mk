2efc10b95fbbaec46dc32ceaa6be910f38c58dcf
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2024-12-08 00:28:19 -0500

    Update lineage_Mi439_4_19.mk

--- lineage_Mi439_4_19.mk
+++ lineage_Mi439_4_19.mk
@@ -36,6 +36,67 @@ TARGET_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Blur
+TARGET_ENABLE_BLUR := true
+
+# BCR (Call recording)
+TARGET_PREBUILT_BCR := true
+
+# Lawnchair (Pixel launcher by default)
+TARGET_INCLUDE_PIXEL_LAUNCHER := true
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+
+# Exclude Aperture camera
+PRODUCT_NO_CAMERA := true
+
+# Google Camera App
+TARGET_PREBUILT_GOOGLE_CAMERA := true
+
+# Custom package installer
+TARGET_USE_CUSTOM_PACKAGE_INSTALLER := true
+
+# Live wallpapers
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+
+# Quick tap
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# Now Playing
+TARGET_SUPPORTS_NOW_PLAYING := true
+
+# Clear Calling
+TARGET_SUPPORTS_CLEAR_CALLING := true
+
+# Call Recording Support
+TARGET_SUPPORTS_CALL_RECORDING := true
+
+# Pixel Carrier Settings
+TARGET_INCLUDE_CARRIER_SETTINGS := true
+TARGET_INCLUDE_PIXEL_IMS := true
+TARGET_INCLUDE_PIXEL_EUICC := true
+TARGET_INCLUDE_CARRIER_SERVICES := true
+
+# A bunch of Pixel stuff
+USE_PIXEL_CHARGER := true
+TARGET_SUPPORTS_DREAMLINER := true
+TARGET_SUPPORTS_GOOGLE_BATTERY := true
+TARGET_SUPPORTS_ADPATIVE_CHARGING := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+
+# GMS
+WITH_GMS := false
+BUILD_GOOGLE_CONTACTS := true
+BUILD_GOOGLE_DIALER := true
+BUILD_GOOGLE_MESSAGE := true
+
+# Mist OS Flags
+MISTOS_MAINTAINER := "GamerBoy1234294"
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="olive-user 10 QKQ1.191014.001 V12.5.1.0.QCNMIXM release-keys"
     BuildFingerprint=Xiaomi/olive/olive:10/QKQ1.191014.001/V12.5.1.0.QCNMIXM:user/release-keys
